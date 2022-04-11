91365fbb1756fdae1b73c1b576ff6a5134a4fb56
harshakmohan <harshakmohan@gmail.com>
2022-04-11 18:27:45 -0400

    created initial file structure

--- src/visualization/viz.py
+++ src/visualization/viz.py
@@ -1,3 +1,5 @@
 '''
-After running inference on a dataset, use this script to generate visualizations of the results
+After running inference on a dataset, use this script to generate visualizations of the results.
+
+Given a sequence of frames and the predictions for those frames, load the frames side by side and play the sequence.
 '''
