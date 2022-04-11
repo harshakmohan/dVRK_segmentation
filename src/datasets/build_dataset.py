a0b6695d8758ef68b1026466c81ec5cc2d2be071
harshakmohan <harshakmohan@gmail.com>
2022-04-11 18:36:20 -0400

    building out build_dataset.py // Dataset classes for the two different datasets

--- src/datasets/build_dataset.py
+++ src/datasets/build_dataset.py
@@ -1,5 +1,34 @@
+import os
+import torch
+import numpy as np
+import torchvision.transforms.functional as TF
+
+from PIL import Image
+from torch.utils.data import Dataset
+
 '''
 Grab data and create all the necessary dataloaders.
 
 All data preprocessing should happen here.
 '''
+
+class UCL(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+
+class EndoVis(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
