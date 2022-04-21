633e15d301611f3c52bd5a4fbbf366d89c6a4b6b
Lydia Zoghbi <lalzogbi@terpmail.umd.edu>
2022-04-21 18:14:59 -0400

    Lydia brainstorming

--- src/visualization/viz.py
+++ src/visualization/viz.py
@@ -1,3 +1,19 @@
+# What I need so that I don't waste time running the model on data that was already passed to it:
+#         - Training dataloader object 
+#         - Validation dataloader object
+#         - Testing dataloader object
+#         - Model predictions on training images with corresponding ground truth masks
+#         - Model predictions on validation images with corresponding ground truth masks
+#         - Class-wise Dice scores on training images (need to match order of training images)
+#         - Class-wise Dice scores on validation images (need to match order of training images)
+
+# What I plan to do with the data
+#         - Modular function for visualizing images with Class-wise highest/lowest Dice scores (for training and validation data) that also outputs scores
+#         - Function for visualizing random test images with predictions
+#         - Function for "sliding" through images so that we can visualiza all images at once without having to display all of them at once (outputs scores when possible)
+
+# If we want to compare the performance of different models together, I can cook up a function for that too (more if needed)
+
 import random
 
 def viz_test_data(dataloader, model):
