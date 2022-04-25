abdd4b5ef2011642e8cafb67896b74da73ce01a9
Gurion Marks <gurion@jhu.edu>
2022-04-25 16:25:02 -0400

    script to binarize endovis labels

--- utils/binarize_endoviz.py
+++ utils/binarize_endoviz.py
@@ -3,6 +3,22 @@ import numpy as np
 import os
 import shutil
 
+def crop_center_np(img,cropx,cropy):
+    y,x = img.shape
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)    
+    return img[starty:starty+cropy,startx:startx+cropx]
+
+def crop_center_img(img, cropx, cropy):
+    width, height = img.size 
+    left = round((width - cropx)/2)
+    top = round((height - cropy)/2)
+    x_right = round(width - cropx) - left
+    x_bottom = round(height - cropy) - top
+    right = width - x_right
+    bottom = height - x_bottom
+
+    return img.crop((left, top, right, bottom))
 
 binary_path = os.path.abspath('../data/binary_endovis')
 dir_exists = os.path.exists(binary_path)
@@ -10,9 +26,13 @@ dir_exists = os.path.exists(binary_path)
 #check for directory and recreate if exists
 if not dir_exists:
     os.makedirs(binary_path)
+    os.mkdir(f'{binary_path}/img')
+    os.mkdir(f'{binary_path}/labels')
 else:
     shutil.rmtree(binary_path)
     os.makedirs(binary_path)
+    os.mkdir(f'{binary_path}/img')
+    os.mkdir(f'{binary_path}/labels')
 
 endovis_path  = os.path.abspath('../data/endovis_dataset/')
 subfolders = [f.path for f in os.scandir(endovis_path) if f.is_dir()]
@@ -23,12 +43,18 @@ for folder in subfolders: #instrument_dataset_x
     gt = os.path.join(root, 'ground_truth')
     gt_folders = [f.path for f in os.scandir(gt) if f.is_dir()]
     for i in range(len(os.listdir(frames))): #for each image in dataset
-        img = f'frame{str(i).zfill(3)}.png'
-        masks = [os.path.join(gt, f'{tool}/{img}') for tool in gt_folders if tool != os.path.abspath(os.path.join(gt, 'Other_labels'))] #masks associated with frame
-        img_array = np.array()
-        for mask in masks:
-            img = np.asarray(Image.open(mask))
-            img_array += img
-        print(img_array) 
-        break
+        img_name = f'frame{str(i).zfill(3)}.png'
+        save_name = f'{folder[-1]}_frame{str(i).zfill(3)}.png'
+        masks = [os.path.join(gt, f'{tool}/{img_name}') for tool in gt_folders if tool != os.path.abspath(os.path.join(gt, 'Other_labels'))] #masks associated with frame
+        images = [np.asarray(Image.open(mask)) for mask in masks]
+        for image in images:
+            image = crop_center_np(image, 701, 538)
+        summed = sum(images)
+        binary_mask = np.where(summed > 0, 255, 0)
+        img_mask = Image.fromarray(binary_mask.astype('uint8'))
+        img_mask.save(f'{binary_path}/labels/{save_name}')
+
+        orig = Image.open(f'{frames}/{img_name}')
+        orig = crop_center_img(orig, 701, 538)
+        orig.save(f'{binary_path}/img/{save_name}')
     break
