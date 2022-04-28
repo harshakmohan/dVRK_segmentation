bc609c4799d76fe0f6c1fd70fa082457daf38097
harsha <harshakmohan@gmail.com>
2022-04-28 11:45:58 -0400

    closing hotfix branch

--- src/run/predict.py
+++ src/run/predict.py
@@ -13,18 +13,21 @@ from .utils import (load_checkpoint, save_checkpoint, check_accuracy)
 from .utils import DiceLoss2D
 import os
 
+
+
 def save_predictions_as_imgs(loader, model, device, folder="predictions/"):
     # TODO: When saving prediction, save with Video_## and image ## in the name of the prediction image file.
     model.eval()
     for idx, (x, y) in enumerate(loader):
         x = x.to(device=device)
         with torch.no_grad():
-            preds = torch.sigmoid(model(x))
+            preds = torch.sigmoid(model(x)).float()
             preds = (preds > 0.5).float()
         torchvision.utils.save_image(
             preds, f"{folder}pred_{idx}.png"
         )
         torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
-
-    model.train()
+        if idx > len(loader)/3:
+            break
+    #model.train()
 
