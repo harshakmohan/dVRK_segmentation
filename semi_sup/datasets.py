2e68c031f6ccf621d97b53a59c281b27ca5fe8b5
lydiazoghbi <lalzogbi@terpmail.umd.edu>
2022-05-04 17:58:38 -0400

    forgot one file

--- semi_sup/datasets.py
+++ semi_sup/datasets.py
@@ -74,6 +74,22 @@ class ImageDataset(Dataset):
 
         return data
 
+    # def get_denormed_images(self, index):
+    #     image = cv2.imread(self.images[index])
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #     mask_3 = cv2.imread(self.gts[index])
+    #     if self.cfg.DATA.SEG_CLASSES == 1:
+    #         mask = mask_3.sum(axis=2) // 384
+    #     else:
+    #         mask = mask_3 // 128
+    #     mask = mask.astype(np.uint8)
+    #     assert mask.max() == 1 or mask.max() == 0
+    #     data_np = self.transform(image=image, mask=mask)
+    #     data_t = self.to_tensors(image=data_np['image'], mask=data_np['mask'])
+    #     data = {'imidx': index, 'path': self.images[index], 'image': data_t['image'], 'label': data_t['mask']}
+    #
+    #     return data
+
     def filter_files(self):
         assert len(self.images) == len(self.gts)
         for img_path, gt_path in zip(self.images, self.gts):
@@ -166,10 +182,12 @@ def get_dataset(mode, cfg, trainsize=538, scale=(0.99, 1)):
         data_root.append(os.path.join(cfg.DIRS.DATA, 'Endovis'))
     if mode == 'train':
         dts = ImageDataset(cfg=cfg, roots=data_root, mode='train', trainsize=trainsize, scale=scale)
+        dts2 = ImageDataset_denormed(cfg=cfg, roots=data_root, mode='train', trainsize=trainsize, scale=scale)
         batch_size = cfg.TRAIN.BATCH_SIZE
 
         total_slices = len(dts)
         # print('==> total_slices', total_slices)
+
         labeled_slice_len = int(total_slices * cfg.DATA.LABEL)
         idxs = list(range(total_slices))
         fold_len = int(cfg.DATA.LABEL * total_slices)
@@ -185,10 +203,17 @@ def get_dataset(mode, cfg, trainsize=538, scale=(0.99, 1)):
                                               batch_size - cfg.TRAIN.LB_BATCH_SIZE)
         dataloader = DataLoader(dts, batch_sampler=batch_sampler,
                                 num_workers=cfg.SYSTEM.NUM_WORKERS, pin_memory=True, worker_init_fn=worker_init_fn)
+
+        dataloader_denormed = DataLoader(dts2, batch_sampler=batch_sampler,
+                                num_workers=cfg.SYSTEM.NUM_WORKERS, pin_memory=True, worker_init_fn=worker_init_fn)
+
     elif mode == 'valid':
         dts = ImageDataset(cfg=cfg, roots=data_root, mode='val', trainsize=trainsize, scale=scale)
         batch_size = cfg.VAL.BATCH_SIZE
         dataloader = DataLoader(dts, batch_size=batch_size,
+                                shuffle=False, drop_last=False,
+                                num_workers=cfg.SYSTEM.NUM_WORKERS)
+        dataloader_denormed = DataLoader(dts, batch_size=batch_size,
                                 shuffle=False, drop_last=False,
                                 num_workers=cfg.SYSTEM.NUM_WORKERS)
     elif mode == 'test':
@@ -198,6 +223,92 @@ def get_dataset(mode, cfg, trainsize=538, scale=(0.99, 1)):
                                 shuffle=False, drop_last=False,
                                 num_workers=cfg.SYSTEM.NUM_WORKERS)
 
+        dataloader_denormed = DataLoader(dts, batch_size=batch_size,
+                                shuffle=False, drop_last=False,
+                                num_workers=cfg.SYSTEM.NUM_WORKERS)
+
     else:
         raise KeyError(f"mode error: {mode}")
-    return dataloader
+    return dataloader, dataloader_denormed
+
+class ImageDataset_denormed(Dataset):
+    """
+    dataloader for polyp segmentation tasks
+    refer to https://github.com/PRIS-CV/DCRNet/blob/master/utils/dataloader.py
+    """
+
+    def __init__(self, cfg, roots, mode='train', trainsize=538, scale=(0.99, 1.01)):
+        self.cfg = cfg
+        self.trainsize = trainsize
+        self.scale = scale
+        self.mode = mode
+        self.images = []
+        self.gts = []
+        self.dataset_lens = []
+        for root in roots:
+            if mode == 'train':
+                self.data_root = os.path.join(root, 'TrainDataset')
+                self.transform = self.get_augmentation()
+            elif mode == 'val':
+                self.data_root = os.path.join(root, 'ValidationDataset')
+                self.transform = A.Compose(
+                    [A.Resize(trainsize, trainsize), ])
+            elif mode == 'test':
+                self.data_root = os.path.join(root, 'TestDataset')
+                self.transform = A.Compose([A.Resize(trainsize, trainsize), ])
+            else:
+                raise KeyError('MODE ERROR')
+            image_root = os.path.join(self.data_root, 'images')
+            gt_root = os.path.join(self.data_root, 'masks')
+            _images = sorted([os.path.join(image_root, f) for f in os.listdir(image_root) if
+                              f.endswith('.jpg') or f.endswith('.png') or f.endswith('.tif')])
+            _gts = sorted([os.path.join(gt_root, f) for f in os.listdir(gt_root) if
+                           f.endswith('.jpg') or f.endswith('.png') or f.endswith('.tif')])
+            self.images += _images
+            self.gts += _gts
+            self.dataset_lens.append(len(self.images))
+        self.filter_files()
+        self.size = len(self.images)
+        self.to_tensors = A.Compose([A.Normalize(), ToTensorV2()])
+
+    def __len__(self):
+        return self.size
+
+    def lens(self):
+        return self.dataset_lens
+
+    def __getitem__(self, index):
+        image = cv2.imread(self.images[index])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask_3 = cv2.imread(self.gts[index])
+        if self.cfg.DATA.SEG_CLASSES == 1:
+            mask = mask_3.sum(axis=2) // 384
+        else:
+            mask = mask_3 // 128
+        mask = mask.astype(np.uint8)
+        assert mask.max() == 1 or mask.max() == 0
+        data_np = self.transform(image=image, mask=mask)
+        aug = ToTensorV2()
+        data_t = aug(image = data_np['image'], mask=data_np['mask'])
+        data = {'imidx': index, 'path': self.images[index], 'image': data_t['image'], 'label': data_t['mask']}
+
+        return data
+
+    def filter_files(self):
+        assert len(self.images) == len(self.gts)
+        for img_path, gt_path in zip(self.images, self.gts):
+            img = cv2.imread(img_path)
+            gt = cv2.imread(gt_path)
+            assert gt.max() == 255
+            assert gt.min() == 0
+            assert img.shape == gt.shape
+            assert img_path.split('/')[-1].split('.')[0].split('_')[0] == \
+                   gt_path.split('/')[-1].split('.')[0].split('_')[0], (img_path, gt_path)
+
+    def get_augmentation(self):
+        return A.Compose([
+            A.Resize(self.trainsize, self.trainsize),
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+            A.RandomRotate90(),
+        ])
